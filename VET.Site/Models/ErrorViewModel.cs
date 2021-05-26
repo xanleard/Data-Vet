db2536bdd2a9c6da496dfdfe2d7e1a24d8a64ed4
Angel.Canales <angel.canales@grupoleitz.com>
2021-05-25 19:30:52 -0600

    Se agrego site

---
+++ VET.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VET.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
