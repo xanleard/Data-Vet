15a0088a686a67f6d4f05d608711ca73e8742402
Rudy Cardona <rudy.cardona@grupoleitz.com>
2021-05-27 23:10:22 -0600

    se agrego la funcionalidad de editar clientes

--- VET.Site/Controllers/CustomerController.cs
+++ VET.Site/Controllers/CustomerController.cs
@@ -116,5 +116,84 @@ namespace VET.Site.Controllers
 
             return this.RedirectToAction("Index");
         }
+
+        [HttpPost]
+        [Authorize(Policy = "RequireAdminRole")]
+        public async Task<IActionResult> Edit(EditCustomersViewModel editModel)
+        {
+            if (editModel == null)
+            {
+                return this.BadRequest();
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(editModel);
+            }
+
+            var customerToEdit = await this.customersManager.FindByIdAsync(editModel.Id);
+
+            if (customerToEdit == null)
+            {
+                return this.NotFound();
+            }
+            customerToEdit.Name = editModel.Name;
+            customerToEdit.IdentificationCard = editModel.IdentificationCard;
+            customerToEdit.BirthDate = editModel.BirthDate.ToString();
+            customerToEdit.Direction = editModel.Direction;
+            customerToEdit.Telephone1 = editModel.Telephone1;
+            customerToEdit.Telephone2 = editModel.Telephone2;
+            customerToEdit.Email = editModel.Email;
+            customerToEdit.CreationDate = editModel.CreationDate;
+            customerToEdit.UpdateDate = editModel.UpdateDate;
+            var editResult = await this.customersManager.EditAsync(customerToEdit);
+
+            if (!editResult.Succeeded)
+            {
+                foreach (var kvp in editResult.ValidationMessages)
+                {
+                    foreach (var message in kvp.Value)
+                    {
+                        this.ModelState.AddModelError(kvp.Key, message);
+                    }
+                }
+
+                return this.View(editModel);
+            }
+
+            return this.RedirectToAction("Index");
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return this.BadRequest();
+            }
+
+            var editList = await this.customersManager.FindByIdAsync(id.Value);
+
+            if (editList == null)
+            {
+                return this.NotFound();
+            }
+
+            var list = new EditCustomersViewModel
+            {
+                Id = editList.Id,
+                Name = editList.Name,
+                IdentificationCard = editList.IdentificationCard,
+                BirthDate = DateTime.ParseExact(editList.BirthDate, "dd/MM/yyyy", null),
+                Direction = editList.Direction,
+                Telephone1 = editList.Telephone1,
+                Telephone2 = editList.Telephone2,
+                Email = editList.Email,
+                CreationDate = editList.CreationDate,
+                UpdateDate = editList.UpdateDate,
+            };
+
+            return this.View(list);
+        }
     }
 }
